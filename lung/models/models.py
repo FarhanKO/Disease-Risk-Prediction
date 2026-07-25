149a96d6ad5790a6fe4049b7bb71a613416ae1ba
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-07-25 12:48:52 +0600

    Update models.py

--- lung/models/models.py
+++ lung/models/models.py
@@ -13,3 +13,25 @@ from tensorflow.keras.models import load_model
 
 MODEL_DIR = "models"
 
+MODEL_PATHS = {
+    "Custom CNN": os.path.join(MODEL_DIR, "Custom_CNN_best.keras"),
+
+    "DenseNet121": os.path.join(MODEL_DIR, "DenseNet121_best.keras"),
+    "DenseNet121 Fine Tuned": os.path.join(
+        MODEL_DIR, "DenseNet121_FT_best.keras"
+    ),
+
+    "EfficientNetB0": os.path.join(
+        MODEL_DIR, "EfficientNetB0_best.keras"
+    ),
+    "EfficientNetB0 Fine Tuned": os.path.join(
+        MODEL_DIR, "EfficientNetB0_FT_best.keras"
+    ),
+
+    "ResNet50": os.path.join(
+        MODEL_DIR, "ResNet50_best.keras"
+    ),
+    "ResNet50 Fine Tuned": os.path.join(
+        MODEL_DIR, "ResNet50_FT_best.keras"
+    )
+}
